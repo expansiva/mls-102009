22992476ee8cc5cd9671061755c0969ed122d741
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2025-08-05 11:44:04 -0300

    delete old files
    delete old files

--- l2/project.test.ts
+++
@@ -1,5 +0,0 @@
-/// <mls shortName="project" project="102009" enhancement="_blank" />
-
- import { ICANTest, ICANIntegration, ICANSchema  } from './_100554_tsTestAST';
- export const integrations: ICANIntegration[] = [];
- export const tests: ICANTest[] = [];
