f1aabc49c140059d08fcc836dc1f58be3b577057
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2025-10-13 10:14:12 -0300

    delet travels dreams
    delet travels dreams

--- l2/traveldreams/module.defs.ts
+++
@@ -1,2 +0,0 @@
-/// <mls shortName="module" project="102009" enhancement="_blank" folder="traveldreams" />
-
--- l2/traveldreams/organismTestimonials.test.ts
+++
@@ -1,5 +0,0 @@
-/// <mls shortName="organismTestimonials" project="102009" enhancement="_blank" folder="traveldreams" />
-
- import { ICANTest, ICANIntegration, ICANSchema  } from './_100554_tsTestAST';
- export const integrations: ICANIntegration[] = [];
- export const tests: ICANTest[] = [];
