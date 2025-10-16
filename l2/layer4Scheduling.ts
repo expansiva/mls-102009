94fc72f1edde297930955cccd205cda4be92c24c
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2025-10-16 16:10:00 -0300

    correções id
    correções id

--- l2/layer4Scheduling.ts
+++ l2/layer4Scheduling.ts
@@ -1,7 +1,7 @@
 /// <mls shortName="layer4Scheduling" project="102009" enhancement="_blank" />
 
 export interface SchedulingData{
-    id?: string,
+    id?: number,
     data: SchedulingRecord,
     version?:string,
 }
@@ -13,9 +13,9 @@ export enum SchedulingStatus {
     'CANCELED' = 'CANCELED'};
 
 export interface SchedulingRecord {
-  clientMdmId: string;
-  petMdmId: string;
-  serviceMdmId: string;
+  clientMdmId: number;
+  petMdmId: number;
+  serviceMdmId: number;
   startDateTime: string; // ISO 8601 datetime (ex: "2025-10-10T16:30:00Z")
   endDateTime?: string;   // ISO 8601 datetime
   status: SchedulingStatus,
