37c10f456cff5433c67551dd65770b73c472b78c
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2025-12-09 14:29:31 -0300

    delete project

--- l2/layer2AddServiceOrder.test.ts
+++
@@ -1,5 +0,0 @@
-/// <mls shortName="layer2AddServiceOrder" project="102009" enhancement="_blank" folder="" />
-
- import { ICANTest, ICANIntegration, ICANSchema  } from './_100554_tsTestAST';
- export const integrations: ICANIntegration[] = [];
- export const tests: ICANTest[] = [];
